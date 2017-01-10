ff49edf0d0d14c63736492a76d1178e3f1c24e4e
Roberto Reale <roberto.reale@linux.com>
2017-01-10 12:11:28 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'sqlite3server',
+    version = '0.0.1',
+    description = 'A SQLite Server in Python 3.',
+    py_modules = [ 'sqlite3server' ],
+    author = 'Roberto Reale',
+    author_email = 'roberto.reale@linux.com',
+    url = 'https://github.com/robertoreale/sqlite3server',
+    keywords = [ 'sqlite3', 'server', ],
+    install_requires = [ 
+    ]
+)
