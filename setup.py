ee72fbe8b0aa5ceeadbb74b6f55f8db879c8a88b
Roberto Reale <rober.reale@gmail.com>
2018-02-07 18:03:16 +0100

    Edit description in setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'sqlite3server',
     version = '0.1',
-    description = 'A SQLite server in Python.',
+    description = 'A SQLite client/server wrapper',
     py_modules = [ 'sqlite3server' ],
     author = 'Roberto Reale',
     author_email = 'rober.reale@gmail.com',
